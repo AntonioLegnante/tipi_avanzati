42cbd322fc934b1a49f90b0a562829f2290f1d46
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-01-17 17:19:07 +0000

    intervalli

---
+++ asterischi.c
@@ -0,0 +1,24 @@
+//stampa degli asterischi a distanza di 700 secondi tra di loro
+
+#include <stdio.h>
+#include <time.h>
+
+int main () 
+{
+    struct timespec intervallo;
+    intervallo.tv_sec = 0;
+    intervallo.tv_nsec = 700 * 1000 * 1000;
+    
+    printf("*\n");
+    nanosleep(&intervallo,NULL);
+   
+    printf("*\n");
+    nanosleep(&intervallo,NULL);
+  
+    printf("*\n");
+    nanosleep(&intervallo,NULL);
+
+return 0;
+}
+    
+    
