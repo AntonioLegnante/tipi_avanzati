82a7f23c2b8a7fa5308ec957b19c7716ea2d6e89
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-01-24 10:33:50 +0000

    somma elementi vettore

--- sumvett.c
+++ sumvett.c
@@ -5,16 +5,15 @@
 int main ()
 {
     int vettore[3], somma=0;
-    float media, somma_float;
+    float media;
 
     printf("inserisci 3 numeri");
     scanf("%d",&vettore[0]);
     scanf("%d",&vettore[1]);
     scanf("%d",&vettore[2]);
-    somma += vettore[0] + vettore[1] + vettore[2];
-    
-    somma_float = (float) somma;
-    media = somma / 3.0;
+    somma = vettore[0] + vettore[1] + vettore[2];
+
+    media = (float) somma / 3.0;
     
     printf("i numeri inseriti sono %d %d %d e la somma tra loro e' %d mentre la loro media e' %f", vettore[0], vettore[1], vettore[2] , somma, media);
 
